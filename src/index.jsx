7ee5581eebd9e2342ead6b16321a9e73fb69e587
Grant Holley <gvholley@gmail.com>
2020-06-29 22:30:02 +0900

    Edited DOM

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import '../assets/stylesheets/application.scss';
-
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
-
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+const Hello = (props) => {
+  return <h1>Hello, {props.name}</h1>;
 }
-<!-- -->
-<!-- -->
-
+const container = document.getElementById('root'); ReactDOM.render(<Hello name="Grant" />, container);
