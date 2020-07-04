c9d4cc912220acd6cf687512a828b6b97e93ce91
Grant Holley <gvholley@gmail.com>
2020-07-04 21:10:50 +0900

    Gif List added

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
 
 class App extends Component {
   render () {
+    const gifs = [
+      { id: "UmGYBusanpEwU" },
+      { id: "12FLhMHdanoLJK" },
+      { id: "8EmeieJAGjvUI" },
+      { id: "DK3nPt4gDanRK" },
+      { id: "h7XVO9OyujKtW" },
+      { id: "DtXfTSHi6mHFS" }
+    ];
+
     return (
       <div>
         <div className="left-scene">
@@ -14,7 +24,9 @@ class App extends Component {
           </div>
         </div>
 
-        <div className="right-scene"></div>
+        <div className="right-scene">
+        <GifList gifs={gifs} />
+        </div>
       </div>
     );
   }
